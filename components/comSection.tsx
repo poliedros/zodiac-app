03e5b95050eb802fc82e81c362d859006e39f169
MrAndersonMR <sr.andersonmendesribeiro@gmail.com>
2023-04-28 16:22:02 -0300

    clean unnecessary comments

--- components/comSection.tsx
+++ components/comSection.tsx
@@ -8,13 +8,6 @@ export default function ComSection(props: {
       {props.type === "item" ? (
         <div className="mx-9 mt-9">
           <div className="flex justify-center flex-col items-center">
-            {/* <Image
-              src="/ramosLogo.png"
-              width={566 / 3}
-              height={836 / 3}
-              alt="ramos logo"
-              className="mb-9"
-            /> */}
             <div className="border-b-4 border-[#AF3838] w-5/6" />
           </div>
           <h2
@@ -23,31 +16,6 @@ export default function ComSection(props: {
           >
             {props.title}
           </h2>
-          {/* <table className="w-full">
-            {props.items.map((item: any, i: number) => (
-              <>
-                <tr key={i} className="text-3xl">
-                  <td className="w-1/6 pr-3">{item.index}.</td>
-                  <td className="w-3/6">
-                    <b>{item.name}</b>
-                  </td>
-                  <td className="w-2/6 text-right pl-3 sm:w-full">
-                    <b>
-                      R$
-                      {/* <span className="text-2xl"> /}
-                      {item.price.split(",")[0]},{/* </span> /}
-                      {item.price.split(",")[1]}
-                    </b>
-                  </td>
-                </tr>
-                <tr key={i} className="text-2xl">
-                  <td className="w-auto"></td>
-                  <td>{item.description}</td>
-                </tr>
-                <div className="pb-4"></div>
-              </>
-            ))}
-          </table> */}
           <div className="w-full">
             {props.items.map((item: any, i: number) => (
               <>
@@ -90,29 +58,6 @@ export default function ComSection(props: {
           >
             {props.title}
           </h2>
-          {/* <table className="w-full text-3xl">
-            {props.items.map((item: any, i: number) => (
-              <>
-                <tr key={i}>
-                  <td className="w-10 text-right pl-6">
-                    <li></li>
-                  </td>
-                  <td>
-                    <b>{item.name}</b>
-                  </td>
-                  <td className="w-20 text-right">
-                    <b>
-                      R$
-                      {/* <span className="text-2xl"> /}
-                      {item.price.split(",")[0]},{/* </span> /}
-                      {item.price.split(",")[1]}
-                    </b>
-                  </td>
-                </tr>
-                <div className="pb-4"></div>
-              </>
-            ))}
-          </table> */}
           <div className="w-full text-3xl">
             {props.items.map((item: any, i: number) => (
               <>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,40 +6,6 @@ import { useState } from "react";
 const GA_TRACKING_ID = "G-KVQPS7V7HZ";
 import Image from "next/image";
 
-function colorfulText(text: string) {
-  return (
-    <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-      {text}
-    </span>
-  );
-}
-
-function Bullet(props: { name: string; price: string }) {
-  const { name, price } = props;
-  return (
-    <li>
-      <b>{name}</b> {price !== "" ? "R$" + price : ""}
-    </li>
-  );
-}
-
-function Item(props: {
-  index: string;
-  name: string;
-  price: string;
-  description: string;
-}) {
-  const { index, name, price, description } = props;
-  return (
-    <div>
-      <p>
-        {index}. <b>{name}</b> R${price}
-      </p>
-      <p>{description}</p>
-    </div>
-  );
-}
-
 function Footer() {
   const today = new Date();
 
@@ -106,13 +72,6 @@ export default function Home() {
       </Head>
 
       <div className="flex flex-col w-screen">
-        {/* <div className="flex">
-          <div className="bg-[#AF3838] w-1/6 h-full bar"></div>
-          <h1 className="mb-4 ml-9 w-full text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-            Cardápio Ramos
-          </h1>
-        </div> */}
-
         {page === undefined ? (
           <div className="flex justify-center flex-col items-center m-6">
             <Image
@@ -151,7 +110,7 @@ export default function Home() {
           </div>
         </div>
 
-        {page !== "" ? (
+        {page !== undefined ? (
           <div className="flex">
             <div className="bg-[#AF3838] w-1/6 h-full bar"></div>
             <div className="w-full">
@@ -264,205 +223,6 @@ export default function Home() {
         ) : (
           <div className="text-2xl">Escolha uma categoria</div>
         )}
-
-        {/* <div>
-          <h2 className="text-4xl font-bold dark:text-white">
-            {colorfulText("DRINKS")}
-          </h2>
-          <div className="space-y-3">
-            {drinks.map((drink) => (
-              <Item
-                key={drink.index}
-                index={drink.index}
-                name={drink.name}
-                price={drink.price}
-                description={drink.description}
-              />
-            ))}
-          </div>
-        </div>
-
-        <div>EM DIA DE MÚSICA AO VIVO COUVERT OBRIGATÓRIO</div>
-
-        <br />
-
-        <div>
-          <h2 className="text-4xl font-bold dark:text-white">
-            {colorfulText("DRINKS DO ZODÍACO")}
-          </h2>
-          <div className="space-y-3">
-            {zodiac.map((drink) => (
-              <Item
-                key={drink.index}
-                index={drink.index}
-                name={drink.name}
-                price={drink.price}
-                description={drink.description}
-              />
-            ))}
-          </div>
-        </div>
-
-        <br />
-
-        <div>
-          <h2 className="text-4xl font-bold dark:text-white">
-            {colorfulText("CARDÁPIO PRIMAVERA & VERÃO")}
-          </h2>
-          <div className="space-y-3">
-            {summer.map((drink) => (
-              <Item
-                key={drink.index}
-                index={drink.index}
-                name={drink.name}
-                price={drink.price}
-                description={drink.description}
-              />
-            ))}
-          </div>
-        </div>
-
-        <br />
-
-        <div>
-          <h2 className="text-4xl font-bold dark:text-white">
-            {colorfulText("SHOTS")}
-          </h2>
-          <div className="space-y-3">
-            {drinkshots.map((drink) => (
-              <Item
-                key={drink.index}
-                index={drink.index}
-                name={drink.name}
-                price={drink.price}
-                description={drink.description}
-              />
-            ))}
-          </div>
-        </div>
-
-        <br />
-
-        <div>
-          <h2 className="text-4xl font-bold dark:text-white">
-            {colorfulText("DRINKS SEM ÁLCOOL")}
-          </h2>
-          <div className="space-y-3">
-            {nonalcoholicdrinks.map((drink) => (
-              <Item
-                key={drink.index}
-                index={drink.index}
-                name={drink.name}
-                price={drink.price}
-                description={drink.description}
-              />
-            ))}
-          </div>
-        </div>
-
-        <br />
-
-        <div>
-          <h2 className="text-4xl font-bold dark:text-white">
-            {colorfulText("CERVEJAS")}
-          </h2>
-          <div className="space-y-3">
-            <ul className="max-w-md space-y-1 list-disc list-inside">
-              {beers.map((beer) => (
-                <Bullet key={beer.name} name={beer.name} price={beer.price} />
-              ))}
-            </ul>
-          </div>
-        </div>
-
-        <br />
-
-        <div>
-          <h2 className="text-4xl font-bold dark:text-white">
-            {colorfulText("BEBIDAS SEM ÁLCOOL")}
-          </h2>
-          <div className="space-y-3">
-            <ul className="max-w-md space-y-1 list-disc list-inside">
-              {nonalcoholic.map((drink) => (
-                <Bullet
-                  key={drink.name}
-                  name={drink.name}
-                  price={drink.price}
-                />
-              ))}
-            </ul>
-          </div>
-        </div>
-
-        <br />
-
-        <div>
-          <h2 className="text-4xl font-bold dark:text-white">
-            {colorfulText("SHOTS")}
-          </h2>
-          <div className="space-y-3">
-            <ul className="max-w-md space-y-1 list-disc list-inside">
-              {shots.map((shot) => (
-                <Bullet key={shot.name} name={shot.name} price={shot.price} />
-              ))}
-            </ul>
-          </div>
-        </div>
-
-        <br />
-
-        <div>
-          <h2 className="text-4xl font-bold dark:text-white">
-            {colorfulText("APERITIVOS")}
-          </h2>
-          <div className="space-y-3">
-            <ul className="max-w-md space-y-1 list-disc list-inside">
-              {appetizer.map((shot) => (
-                <Bullet key={shot.name} name={shot.name} price={shot.price} />
-              ))}
-            </ul>
-          </div>
-        </div>
-
-        <br />
-
-        <div>
-          <h2 className="text-4xl font-bold dark:text-white">
-            {colorfulText("PORÇÕES")}
-          </h2>
-          <div className="space-y-3">
-            {portions.map((portion) => (
-              <Item
-                key={portion.index}
-                index={portion.index}
-                name={portion.name}
-                price={portion.price}
-                description={portion.description}
-              />
-            ))}
-          </div>
-        </div>
-
-        <div>*MEIA PORÇÃO SERÁ COBRADO O VALOR DE 70%*</div>
-
-        <br />
-
-        <div>
-          <h2 className="text-4xl font-bold dark:text-white">
-            {colorfulText("PORÇÕES INDIVIDUAIS")}
-          </h2>
-          <div className="space-y-3">
-            {individualportions.map((portion) => (
-              <Item
-                key={portion.index}
-                index={portion.index}
-                name={portion.name}
-                price={portion.price}
-                description={portion.description}
-              />
-            ))}
-          </div>
-        </div> */}
         <div className="my-6 flex justify-center">
           <div className="border-b-4 border-[#AF3838] w-5/6" />
         </div>
