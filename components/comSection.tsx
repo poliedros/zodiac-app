8bef751ed9bfeedb81158fffe21addb2dc20ce9d
MrAndersonMR <sr.andersonmendesribeiro@gmail.com>
2023-04-28 15:50:08 -0300

    fix layout like the client wants

---
+++ components/comSection.tsx
@@ -0,0 +1,148 @@
+export default function ComSection(props: {
+  type: string;
+  title: string;
+  items: any;
+}) {
+  return (
+    <>
+      {props.type === "item" ? (
+        <div className="mx-9 mt-9">
+          <div className="flex justify-center flex-col items-center">
+            {/* <Image
+              src="/ramosLogo.png"
+              width={566 / 3}
+              height={836 / 3}
+              alt="ramos logo"
+              className="mb-9"
+            /> */}
+            <div className="border-b-4 border-[#AF3838] w-5/6" />
+          </div>
+          <h2
+            style={{ fontFamily: "'DM Serif Display', serif" }}
+            className="capitalize text-center text-6xl my-9"
+          >
+            {props.title}
+          </h2>
+          {/* <table className="w-full">
+            {props.items.map((item: any, i: number) => (
+              <>
+                <tr key={i} className="text-3xl">
+                  <td className="w-1/6 pr-3">{item.index}.</td>
+                  <td className="w-3/6">
+                    <b>{item.name}</b>
+                  </td>
+                  <td className="w-2/6 text-right pl-3 sm:w-full">
+                    <b>
+                      R$
+                      {/* <span className="text-2xl"> /}
+                      {item.price.split(",")[0]},{/* </span> /}
+                      {item.price.split(",")[1]}
+                    </b>
+                  </td>
+                </tr>
+                <tr key={i} className="text-2xl">
+                  <td className="w-auto"></td>
+                  <td>{item.description}</td>
+                </tr>
+                <div className="pb-4"></div>
+              </>
+            ))}
+          </table> */}
+          <div className="w-full">
+            {props.items.map((item: any, i: number) => (
+              <>
+                <div key={i} className="text-3xl flex line">
+                  <div className="flex w-4/6">
+                    <div className="pr-3">{item.index}.</div>
+                    <div className="">
+                      <b>{item.name}</b>
+                    </div>
+                  </div>
+                  <div className="w-2/6 text-right pl-3 price">
+                    <b>
+                      {item.price.split(",")[0].length !== 0
+                        ? "R$" +
+                          /* <span className="text-2xl"> */
+                          item.price.split(",")[0] +
+                          "," /* </span> */ +
+                          item.price.split(",")[1]
+                        : item.price}
+                    </b>
+                  </div>
+                </div>
+                <div key={i} className="text-2xl">
+                  <td className="w-auto"></td>
+                  <td>{item.description}</td>
+                </div>
+                <div className="pb-4"></div>
+              </>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="mx-9 mt-9">
+          <div className="flex justify-center flex-col items-center">
+            <div className="border-b-4 border-[#AF3838] w-5/6" />
+          </div>
+          <h2
+            style={{ fontFamily: "'DM Serif Display', serif" }}
+            className="capitalize text-center text-6xl my-9"
+          >
+            {props.title}
+          </h2>
+          {/* <table className="w-full text-3xl">
+            {props.items.map((item: any, i: number) => (
+              <>
+                <tr key={i}>
+                  <td className="w-10 text-right pl-6">
+                    <li></li>
+                  </td>
+                  <td>
+                    <b>{item.name}</b>
+                  </td>
+                  <td className="w-20 text-right">
+                    <b>
+                      R$
+                      {/* <span className="text-2xl"> /}
+                      {item.price.split(",")[0]},{/* </span> /}
+                      {item.price.split(",")[1]}
+                    </b>
+                  </td>
+                </tr>
+                <div className="pb-4"></div>
+              </>
+            ))}
+          </table> */}
+          <div className="w-full text-3xl">
+            {props.items.map((item: any, i: number) => (
+              <>
+                <div key={i} className="flex line">
+                  <div className="flex w-3/6">
+                    <div className="text-right pl-6">
+                      <li></li>
+                    </div>
+                    <div>
+                      <b>{item.name}</b>
+                    </div>
+                  </div>
+                  <div className="w-3/6 text-right price">
+                    <b>
+                      {item.price.split(",")[0].length < 10
+                        ? "R$" +
+                          /* <span className="text-2xl"> */
+                          item.price.split(",")[0] +
+                          "," /* </span> */ +
+                          item.price.split(",")[1]
+                        : item.price}
+                    </b>
+                  </div>
+                </div>
+                <div className="pb-4"></div>
+              </>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Script from "next/script";
 import catalog from "../catalog.json";
+import ComSection from "@/components/comSection";
+import { useState } from "react";
 const GA_TRACKING_ID = "G-KVQPS7V7HZ";
+import Image from "next/image";
 
 function colorfulText(text: string) {
   return (
@@ -38,13 +41,31 @@ function Item(props: {
 }
 
 function Footer() {
+  const today = new Date();
+
   return (
     <footer>
-      <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        Desenvolvido em São João del-Rei - 2023{" "}
-        <a href="https://czar.dev/" className="hover:underline">
-          pela CZAR+™
-        </a>
+      <span className="flex m-3 items-center justify-evenly text-lg text-gray-500 sm:text-center dark:text-gray-400">
+        Desenvolvido por:{" "}
+        <div className="flex">
+          <div className="bg-[#282C35] rounded-full w-[85px] h-[85px] mx-3 flex items-center justify-center">
+            <a
+              style={{ fontFamily: "'Josefin Sans', sans-serif" }}
+              href="https://czar.dev/"
+              className="hover:underline"
+            >
+              <Image
+                src={"/czarLogo.png"}
+                alt={"Czar+"}
+                width={270 / 4}
+                height={140 / 4}
+              />
+            </a>
+          </div>
+        </div>
+        <div className="text-right">
+          São João del-Rei - MG | {today.getFullYear()}
+        </div>
       </span>
     </footer>
   );
@@ -62,6 +83,8 @@ export default function Home() {
   const appetizer = catalog["appetizer"];
   const portions = catalog["portions"];
   const individualportions = catalog["individualportions"];
+
+  const [page, setPage] = useState<string>();
 
   return (
     <div className="flex justify-center">
@@ -82,12 +105,157 @@ export default function Home() {
         <title>Cardápio Ramos</title>
       </Head>
 
-      <div className="flex flex-col">
-        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-          Cardápio Ramos
-        </h1>
+      <div className="flex flex-col w-screen">
+        {/* <div className="flex">
+          <div className="bg-[#AF3838] w-1/6 h-full bar"></div>
+          <h1 className="mb-4 ml-9 w-full text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            Cardápio Ramos
+          </h1>
+        </div> */}
 
-        <div>
+        {page === undefined ? (
+          <div className="flex justify-center flex-col items-center m-6">
+            <Image
+              src="/ramosLogo.png"
+              width={566 / 4}
+              height={836 / 4}
+              alt="ramos logo"
+              className="my-9"
+            />
+          </div>
+        ) : (
+          <></>
+        )}
+
+        <div className="flex">
+          <div className="w-1/6 h-full bar"></div>
+          <div className="flex justify-evenly text-center my-3 w-full">
+            <button
+              className="bg-[#AF3838] p-2 text-white uppercase hover:bg-[black]"
+              onClick={() => setPage("drinks")}
+            >
+              drinks
+            </button>
+            <button
+              className="bg-[#AF3838] p-2 text-white uppercase hover:bg-[black]"
+              onClick={() => setPage("drinks2")}
+            >
+              bebidas
+            </button>
+            <button
+              className="bg-[#AF3838] p-2 text-white uppercase hover:bg-[black]"
+              onClick={() => setPage("portions")}
+            >
+              porções
+            </button>
+          </div>
+        </div>
+
+        {page !== "" ? (
+          <div className="flex">
+            <div className="bg-[#AF3838] w-1/6 h-full bar"></div>
+            <div className="w-full">
+              {page === "drinks" ? (
+                <>
+                  <div className="flex justify-center flex-col items-center">
+                    <Image
+                      src="/ramosLogo.png"
+                      width={566 / 3}
+                      height={836 / 3}
+                      alt="ramos logo"
+                      className="mt-9"
+                    />
+                  </div>
+                  <ComSection type={"item"} title={"drinks"} items={drinks} />
+                  <div>EM DIA DE MÚSICA AO VIVO COUVERT OBRIGATÓRIO</div>
+                  <ComSection
+                    type={"item"}
+                    title={"drinks do zodíaco"}
+                    items={zodiac}
+                  />
+                  <ComSection
+                    type={"item"}
+                    title={"cardápio primavera & verão"}
+                    items={summer}
+                  />
+                </>
+              ) : (
+                <></>
+              )}
+              {page === "drinks2" ? (
+                <>
+                  <div className="flex justify-center flex-col items-center">
+                    <Image
+                      src="/ramosLogo.png"
+                      width={566 / 3}
+                      height={836 / 3}
+                      alt="ramos logo"
+                      className="mt-9"
+                    />
+                  </div>
+                  <ComSection
+                    type={"item"}
+                    title={"shots"}
+                    items={drinkshots}
+                  />
+                  <ComSection
+                    type={"item"}
+                    title={"drinks sem álcool"}
+                    items={nonalcoholicdrinks}
+                  />
+                  <ComSection
+                    type={"bullet"}
+                    title={"cervejas"}
+                    items={beers}
+                  />
+                  <ComSection
+                    type={"bullet"}
+                    title={"bebidas sem álcool"}
+                    items={nonalcoholic}
+                  />
+                  <ComSection type={"bullet"} title={"doses"} items={shots} />
+                  <ComSection
+                    type={"bullet"}
+                    title={"aperitivos"}
+                    items={appetizer}
+                  />
+                </>
+              ) : (
+                <></>
+              )}
+              {page === "portions" ? (
+                <>
+                  <div className="flex justify-center flex-col items-center">
+                    <Image
+                      src="/ramosLogo.png"
+                      width={566 / 3}
+                      height={836 / 3}
+                      alt="ramos logo"
+                      className="mt-9"
+                    />
+                  </div>
+                  <ComSection
+                    type={"item"}
+                    title={"porções"}
+                    items={portions}
+                  />
+                  <div>*MEIA PORÇÃO SERÁ COBRADO O VALOR DE 70%*</div>
+                  <ComSection
+                    type={"item"}
+                    title={"porções individuais"}
+                    items={individualportions}
+                  />
+                </>
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
+        ) : (
+          <div className="text-2xl">Escolha uma categoria</div>
+        )}
+
+        {/* <div>
           <h2 className="text-4xl font-bold dark:text-white">
             {colorfulText("DRINKS")}
           </h2>
@@ -284,8 +452,10 @@ export default function Home() {
               />
             ))}
           </div>
+        </div> */}
+        <div className="my-6 flex justify-center">
+          <div className="border-b-4 border-[#AF3838] w-5/6" />
         </div>
-
         <Footer />
       </div>
     </div>
